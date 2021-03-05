80202b3ab777fbd14a360897efbb70e5bc7bceaa
Thomas Singer <thomas@singer-software.com>
2021-03-05 17:29:35 +0100

    (test) todo

---
+++ __tests__/open-template.test.ts
@@ -0,0 +1,9 @@
+// import { Presentation } from "../src/template"
+
+// test("open pptx template file", () => {
+//   const myTemplate = new Presentation(`${__dirname}/../pptx/Presentation.pptx`)
+
+//   expect(myTemplate).toBeInstanceOf(Presentation)
+
+//   myTemplate.read()
+// });
