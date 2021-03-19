7c7d665066cd5d4b3fc005ad38f8002aad24089a
Thomas Singer <tsinger@gmx.de>
2021-03-19 12:37:18 +0100

    feat: export Automizer.modify functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import Automizer from "./automizer"
 
 export default Automizer
+
+import { 
+  setSolidFill,
+  setText,
+  setPosition,
+  setAttribute,
+  setChartData,
+  dump
+} from "./helper/modify"
+
+export const modify = { 
+  setSolidFill,
+  setText,
+  setPosition,
+  setAttribute,
+  setChartData,
+  dump
+}
