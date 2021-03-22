b2c706de46e44f6459d469a288c40f5c8deb8818
Thomas Singer <tsinger@gmx.de>
2021-03-22 14:56:48 +0100

    refactor: add eslint; remove unused dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    es6: true,
+    jest: true,
+    node: true
+  }
+};
