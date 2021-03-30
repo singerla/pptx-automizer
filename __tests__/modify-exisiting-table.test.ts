4e28737fd7bc74c4be0757071cdd53c2a3038495
Thomas Singer <tsinger@gmx.de>
2021-03-30 10:05:05 +0200

    test: modify an existing table

---
+++ __tests__/modify-exisiting-table.test.ts
@@ -0,0 +1,56 @@
+import Automizer from '../src/index';
+
+test('create presentation, add and modify an existing table.', async () => {
+  const automizer = new Automizer({
+    templateDir: `${__dirname}/pptx-templates`,
+    outputDir: `${__dirname}/pptx-output`,
+  });
+
+  const data = [
+    [
+      { label: 'my header 1' },
+      { label: 'my header 2' },
+      { label: 'my header 3' },
+      { label: 'my header 4' }
+    ],
+    [
+      { label: 'my cell 1-1' },
+      { label: 'my cell 1-2' },
+    ],
+    [], // we don't want to change body row 2
+    [
+      {}, // we also want to skip body row3/col1 and row3/col2
+      {},
+      { label: 'my cell 3-3' },
+      { label: 'my cell 3-4' },
+    ]
+  ]
+
+  const pres = automizer
+    .loadRoot(`RootTemplate.pptx`)
+    .load(`SlideWithTable.pptx`, 'tables');
+
+  const result = await pres
+    .addSlide('tables', 1, (slide) => {
+      slide.modifyElement('TableHeader', (table) => {
+        
+        // uncomment next line to dump table's xml in console 
+        // modify.dump(table)
+        
+        data.forEach((row,r) => {
+          const tabRow = table.getElementsByTagName('a:tr')[r]
+          row.forEach((cell,c) => {
+            const tabCell = tabRow.getElementsByTagName('a:tc')[c]
+
+            // We can hopefully found a corresponding text node
+            // and update textContent by our own label.
+            tabCell.getElementsByTagName('a:t')
+              [0].firstChild.textContent = String(cell.label)
+          })
+        })
+      });
+    })
+    .write(`modify-existing-table.test.pptx`);
+
+  // expect(result.tables).toBe(2); // tbd
+});
