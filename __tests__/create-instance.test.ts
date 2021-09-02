c82ba33bb674ee6b7a8460b1e1596aa2f0dc5e18
singerla <tsinger@gmx.de>
2021-09-02 08:13:46 +0200

    test(params): add initial params to automizer constructor

--- __tests__/create-instance.test.ts
+++ __tests__/create-instance.test.ts
@@ -1,7 +1,10 @@
 import Automizer from '../src/index';
 
 test('create automizer instance', () => {
-  const automizer = new Automizer();
+  const automizer = new Automizer({
+    templateDir: `./pptx-templates`,
+    outputDir: `./pptx-output`,
+  });
 
   expect(automizer).toBeInstanceOf(Automizer);
 });
--- __tests__/error-template-not-found.test.ts
+++ __tests__/error-template-not-found.test.ts
@@ -1,7 +1,10 @@
 import Automizer from '../src/index';
 
 test('throw error if template file not found', () => {
-  const automizer = new Automizer();
+  const automizer = new Automizer({
+    templateDir: `./pptx-templates`,
+    outputDir: `./pptx-output`,
+  });
 
   expect(() => {
     automizer.load(`non/existing/Template.pptx`);
