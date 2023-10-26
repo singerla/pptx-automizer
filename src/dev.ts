97e4ab9651e1d19414f5d6f43a775ba7d9077a5c
singerla <tsinger@gmx.de>
2023-10-26 21:48:31 +0200

    feature(use): add pptxgen to deps; basic implementation #60

--- src/dev.ts
+++ src/dev.ts
@@ -5,58 +5,44 @@ import Automizer, {
   ModifyShapeHelper,
   ModifyTextHelper,
 } from './index';
+import { vd } from './helper/general-helper';
+import pptxgen from 'pptxgenjs';
 
 const run = async () => {
+  const outputDir = `${__dirname}/../__tests__/pptx-output`;
+  const templateDir = `${__dirname}/../__tests__/pptx-templates`;
+
+  let presPptxGen = new pptxgen();
+
+  let slide = presPptxGen.addSlide();
+  let textboxText = 'Hello World from PptxGenJS!';
+  let textboxOpts: pptxgen.TextPropsOptions = {
+    x: 1,
+    y: 1,
+    color: '363636',
+    objectName: 'Text 1',
+  };
+  slide.addText(textboxText, textboxOpts);
+
+  await presPptxGen.writeFile({
+    fileName: templateDir + '/presPptxGenTmp.pptx',
+  });
+
   const automizer = new Automizer({
-    templateDir: `${__dirname}/../__tests__/pptx-templates`,
-    outputDir: `${__dirname}/../__tests__/pptx-output`,
+    templateDir,
+    outputDir,
     removeExistingSlides: true,
   });
 
   let pres = automizer
-    .loadRoot(`SlideWithShapes.pptx`)
-    // We load it twice to make it available for modifying slides
-    // Defining a "name" as second params makes it a little easier
-    .load(`SlideWithShapes.pptx`, 'myTemplate');
+    .loadRoot(`RootTemplate.pptx`)
+    .load(`presPptxGenTmp.pptx`, 'presPptxGenTmp')
+    .load(`SlideWithShapes.pptx`, 'shapes');
 
-  // This is brandnew: get useful information about loaded templates:
-  const myTemplates = await pres.getInfo();
-  const mySlides = myTemplates.slidesByTemplate(`myTemplate`);
-
-  // Feel free to create some functions to pre-define all modifications
-  // you need to apply to your slides.
-  type CallbackBySlideNumber = {
-    slideNumber: number;
-    callback: (slide: ISlide) => void;
-  };
-  const callbacks: CallbackBySlideNumber[] = [
-    {
-      slideNumber: 2,
-      callback: (slide: ISlide) => {
-        slide.modifyElement('Cloud', [
-          ModifyTextHelper.setText('My content'),
-          ModifyShapeHelper.setPosition({
-            h: CmToDxa(5),
-          }),
-          ModifyColorHelper.solidFill({
-            type: 'srgbClr',
-            value: 'cccccc',
-          }),
-        ]);
-      },
-    },
-  ];
-  const getCallbacks = (slideNumber: number) => {
-    return callbacks.find((callback) => callback.slideNumber === slideNumber)
-      ?.callback;
-  };
-
-  // We can loop all slides an apply the callbacks if defined
-  mySlides.forEach((mySlide) => {
-    pres.addSlide('myTemplate', mySlide.number, getCallbacks(mySlide.number));
+  pres.addSlide('shapes', 1, (slide) => {
+    slide.addElement('presPptxGenTmp', 1, 'Text 1');
   });
 
-  // This will result to an output presentation containing all slides of "SlideWithShapes.pptx"
   pres.write(`myOutputPresentation.pptx`).then((summary) => {
     console.log(summary);
   });
