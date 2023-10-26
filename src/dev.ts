adfe37f20c25cfa3c80ed950f03c78fefbce1e4a
singerla <tsinger@gmx.de>
2023-10-26 21:48:31 +0200

    feature(use): add pptxgen to deps; basic implementation #60

--- src/dev.ts
+++ src/dev.ts
@@ -1,75 +1,46 @@
-import Automizer, { ChartData, modify } from './index';
+import Automizer, {
+  CmToDxa,
+  ISlide,
+  ModifyColorHelper,
+  ModifyShapeHelper,
+  ModifyTextHelper,
+} from './index';
+import { vd } from './helper/general-helper';
+import pptxgen from 'pptxgenjs';
 
 const run = async () => {
+  const outputDir = `${__dirname}/../__tests__/pptx-output`;
+  const templateDir = `${__dirname}/../__tests__/pptx-templates`;
+
+  let presPptxGen = new pptxgen();
+
+  let slide = presPptxGen.addSlide();
+  let textboxText = 'Hello World from PptxGenJS!';
+  let textboxOpts: pptxgen.TextPropsOptions = {
+    x: 1,
+    y: 1,
+    color: '363636',
+    objectName: 'Text 1',
+  };
+  slide.addText(textboxText, textboxOpts);
+
+  await presPptxGen.writeFile({
+    fileName: templateDir + '/presPptxGenTmp.pptx',
+  });
+
   const automizer = new Automizer({
-    templateDir: `${__dirname}/../__tests__/pptx-templates`,
-    outputDir: `${__dirname}/../__tests__/pptx-output`,
+    templateDir,
+    outputDir,
     removeExistingSlides: true,
   });
 
   let pres = automizer
     .loadRoot(`RootTemplate.pptx`)
-    .load(`SlideWithCharts.pptx`, 'charts')
-    .load(`EmptySlide.pptx`, 'emptySlide')
+    .load(`presPptxGenTmp.pptx`, 'presPptxGenTmp')
+    .load(`SlideWithShapes.pptx`, 'shapes');
 
-  pres.addSlide('emptySlide', 1, async (slide) => {
-    slide.addElement('chart', 3, '33174534-89bf-4326-8085-4b6938d36f7d');
-    // slide.addElement('charts', 2, 'ColumnChart');
-
-    // slide.generate((pptxGenJSSlide, objectName, pptxGenJs) => {
-    //   let dataChartAreaLine = [
-    //     {
-    //       name: 'Actual Sales',
-    //       labels: [
-    //         'Jan',
-    //         'Feb',
-    //         'Mar',
-    //         'Apr',
-    //         'May',
-    //         'Jun',
-    //         'Jul',
-    //         'Aug',
-    //         'Sep',
-    //         'Oct',
-    //         'Nov',
-    //         'Dec',
-    //       ],
-    //       values: [
-    //         1500, 4600, 5156, 3167, 8510, 8009, 6006, 7855, 12102, 12789, 10123,
-    //         15121,
-    //       ],
-    //     },
-    //     {
-    //       name: 'Projected Sales',
-    //       labels: [
-    //         'Jan',
-    //         'Feb',
-    //         'Mar',
-    //         'Apr',
-    //         'May',
-    //         'Jun',
-    //         'Jul',
-    //         'Aug',
-    //         'Sep',
-    //         'Oct',
-    //         'Nov',
-    //         'Dec',
-    //       ],
-    //       values: [
-    //         1000, 2600, 3456, 4567, 5010, 6009, 7006, 8855, 9102, 10789, 11123,
-    //         12121,
-    //       ],
-    //     },
-    //   ];
-    //
-    //   pptxGenJSSlide.addChart('line', dataChartAreaLine, {
-    //     x: 1,
-    //     y: 1,
-    //     w: 8,
-    //     h: 4,
-    //     objectName,
-    //   });
-    // });
+  pres.addSlide('shapes', 1, (slide) => {
+    slide.addElement('presPptxGenTmp', 1, 'Text 1');
   });
 
   pres.write(`myOutputPresentation.pptx`).then((summary) => {
