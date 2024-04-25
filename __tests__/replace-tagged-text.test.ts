7c0f2b50d700c9f0a00942649c1a68f56ba21608
singerla <tsinger@gmx.de>
2024-04-25 10:42:42 +0200

    test(text): use replaceText to set bulleted lists; see #101

--- __tests__/replace-tagged-text.test.ts
+++ __tests__/replace-tagged-text.test.ts
@@ -1,67 +1,93 @@
 import Automizer, { modify } from '../src/index';
 
+const bulletPoints = ['first line', 'second line', 'third line'].join(`
+`);
+
 test('create presentation, replace tagged text.', async () => {
   const automizer = new Automizer({
     templateDir: `${__dirname}/pptx-templates`,
     outputDir: `${__dirname}/pptx-output`,
   });
 
-  const pres = automizer
-    .loadRoot(`RootTemplate.pptx`)
-    .load(`TextReplace.pptx`)
+  const pres = automizer.loadRoot(`RootTemplate.pptx`).load(`TextReplace.pptx`);
 
   const result = await pres
     .addSlide('TextReplace.pptx', 1, (slide) => {
-      slide.modifyElement(
-        'setText',
-        modify.setText('Test')
-      )
+      slide.modifyElement('setText', modify.setText('Test'));
 
       slide.modifyElement(
         'replaceText',
-        modify.replaceText([
-          {
-            replace: 'replace',
-            by: {
-              text: 'Apples'
-            }
-          },
-          {
-            replace: 'by',
-            by: {
-              text: 'Bananas'
-            }
-          },
-          {
-            replace: 'replacement',
-            by: [
-              {
-                text: 'Really!',
-                style: {
-                  size: 10000,
-                  color: {
-                    type: 'srgbClr',
-                    value: 'ccaa4f'
-                  }
-                }
+        modify.replaceText(
+          [
+            {
+              replace: 'replace',
+              by: {
+                text: 'Apples',
               },
-              {
-                text: 'Fine!',
-                style: {
-                  size: 10000,
-                  color: {
-                    type: 'schemeClr',
-                    value: 'accent2'
-                  }
-                }
-              }
-            ]
+            },
+            {
+              replace: 'by',
+              by: {
+                text: 'Bananas',
+              },
+            },
+            {
+              replace: 'replacement',
+              by: [
+                {
+                  text: 'Really!',
+                  style: {
+                    size: 10000,
+                    color: {
+                      type: 'srgbClr',
+                      value: 'ccaa4f',
+                    },
+                  },
+                },
+                {
+                  text: 'Fine!',
+                  style: {
+                    size: 10000,
+                    color: {
+                      type: 'schemeClr',
+                      value: 'accent2',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+          {
+            openingTag: '{{',
+            closingTag: '}}',
           },
-        ], {
-          openingTag: '{{',
-          closingTag: '}}'
-        })
-      )
+        ),
+      );
+    })
+    .addSlide('TextReplace.pptx', 2, (slide) => {
+      slide.modifyElement(
+        'replaceTextBullet1',
+        modify.replaceText(
+          [
+            {
+              replace: 'bullet1',
+              by: {
+                text: bulletPoints,
+              },
+            },
+            {
+              replace: 'bullet2',
+              by: {
+                text: bulletPoints,
+              },
+            },
+          ],
+          {
+            openingTag: '{{',
+            closingTag: '}}',
+          },
+        ),
+      );
     })
     .write(`replace-tagged-text.test.pptx`);
 
