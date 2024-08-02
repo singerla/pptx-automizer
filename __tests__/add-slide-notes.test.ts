a9e5ec4340bbd7d253a31d4857104f0f421bd150
singerla <tsinger@gmx.de>
2024-08-02 09:49:51 +0200

    chore(test): add example for #113

--- __tests__/add-slide-notes.test.ts
+++ __tests__/add-slide-notes.test.ts
@@ -1,15 +1,22 @@
 import Automizer from '../src/automizer';
+import { vd } from '../src/helper/general-helper';
 
 test('create presentation and append slides with notes', async () => {
   const automizer = new Automizer({
     templateDir: `${__dirname}/pptx-templates`,
-    outputDir: `${__dirname}/pptx-output`
+    outputDir: `${__dirname}/pptx-output`,
   });
 
-  const pres = automizer.loadRoot(`RootTemplate.pptx`)
+  const pres = automizer
+    .loadRoot(`RootTemplate.pptx`)
     .load(`SlideWithNotes.pptx`, 'notes');
 
   pres.addSlide('notes', 1);
+
+  const creationIds = await pres.setCreationIds();
+  // This will print out the first line of a slide note field
+  // OR the slide title, if there is no slide note present:
+  console.log(creationIds[0].slides[0].info);
 
   const result = await pres.write(`add-slide-notes.test.pptx`);
 
