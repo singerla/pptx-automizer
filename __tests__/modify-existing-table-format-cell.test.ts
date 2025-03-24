11472f2e2f3840fdc3020c0d2365060908406704
singerla <tsinger@gmx.de>
2025-03-24 10:48:43 +0100

    chore(test): apply custom callback to table cell for additional formatting

--- __tests__/modify-existing-table-format-cell.test.ts
+++ __tests__/modify-existing-table-format-cell.test.ts
@@ -1,4 +1,10 @@
-import Automizer, { modify, TableData } from '../src/index';
+import Automizer, {
+  modify,
+  TableData,
+  XmlElement,
+  XmlHelper,
+} from '../src/index';
+import ModifyColorHelper from '../src/helper/modify-color-helper';
 
 test('Add and modify an existing table, apply styles to cell.', async () => {
   const automizer = new Automizer({
@@ -23,6 +29,12 @@ test('Add and modify an existing table, apply styles to cell.', async () => {
             isItalics: true,
             isBold: true,
             size: 1200,
+          },
+          {
+            background: {
+              type: 'srgbClr',
+              value: 'FFFFFF',
+            },
           },
         ],
       },
@@ -133,11 +145,60 @@ test('Add and modify an existing table, apply styles to cell.', async () => {
     .loadRoot(`RootTemplate.pptx`)
     .load(`SlideWithTables.pptx`, 'tables');
 
+  // Retrieve all text contents from a given table cell:
+  const parseCellText = (cell: XmlElement) => {
+    const texts = cell.getElementsByTagName('a:t');
+    const textFragments = [];
+    for (let t = 0; t < texts.length; t++) {
+      textFragments.push(texts.item(t).textContent);
+    }
+    return textFragments.join('');
+  };
+
+  // Iterate all rows and columns and appy a callback on each table cell:
+  const modifyTableCell = (
+    element: XmlElement,
+    callback: (cell: XmlElement) => void,
+  ) => {
+    const rows = element.getElementsByTagName('a:tr');
+    XmlHelper.modifyCollection(rows, (row) => {
+      const columns = row.getElementsByTagName('a:tc');
+      XmlHelper.modifyCollection(columns, (cell: XmlElement) => {
+        callback(cell);
+      });
+    });
+  };
+
   const result = await pres
     .addSlide('tables', 3, (slide) => {
       slide.modifyElement('TableWithEmptyCells', [
+        // First, set up table with data & style
         modify.setTable(data1),
+
+        // Show the table XML after setTable modification was done:
         // modify.dump
+
+        // Apply a custom modifier if you require additional formatting:
+        (element: XmlElement) => {
+          modifyTableCell(element, (cell: XmlElement) => {
+            // Get text from all text elements in current cell:
+            const text = parseCellText(cell);
+
+            // Do your checks:
+            if (text === 'test1' || text === '11') {
+              // Log cell XML to console:
+              // XmlHelper.dump(cell);
+
+              ModifyColorHelper.solidFill(
+                {
+                  type: 'srgbClr',
+                  value: '#FF3300',
+                },
+                'last',
+              )(cell);
+            }
+          });
+        },
       ]);
     })
     .write(`modify-existing-table-format-cells.test.pptx`);
